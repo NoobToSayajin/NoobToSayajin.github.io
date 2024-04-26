15a55e0412ca010d8a8a98c824167673c720dd23
noob <noob.to.sayajin@gmail.com>
2024-04-27 01:31:18 +0200

    correction pdf

--- scripts/projetElement.js
+++ scripts/projetElement.js
@@ -1,6 +1,6 @@
 if (window.screen.width >= 1023)
 {
-    let inex = document.getElementById("index")
+    let index = document.getElementById("index")
     index.setAttribute("open", "open")
 }
 document.querySelector(".pdfProjet").onclick = function() {window.open("/pdf/Cahier_des_charges.pdf", "_blank");}
